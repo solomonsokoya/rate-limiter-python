a704ca5aed70abab88924da8152d865cdc5fbb28
Solomon sokoya <solomon@democracy.works>
2023-11-11 19:14:22 -0500

    fix: testing and space

--- test_app.py
+++ test_app.py
@@ -5,26 +5,20 @@ from tokenbucket import Token_Bucket, ips
 import time
 
 class TestTokenBucketApp(unittest.TestCase):
-    def test_limitedtb_rate_limiting(self):
-        res = []
-        client = app.test_client()
-        ip = "127.0.0.1"  # Replace with a valid IP address.
-
-        for _ in range(11):
-            response = client.get('/limitedtb', headers={'x-test-ip': ip})
-            res.append(response.status_code)
+	def test_limitedtb_rate_limiting(self):
+		res = []
+		client = app.test_client()
+		ip = "127.0.0.1"  # Replace with a valid IP address.
+		
+		for _ in range(11):
+			response = client.get('/limitedtb', headers={'x-test-ip': ip})
+			res.append(response.status_code)
 
 		ip2 = "128.0.0.1"  # Replace with a valid IP address.
-
-        for _ in range(8):
-            response = client.get('/limitedtb', headers={'x-test-ip': ip2})
-            res.append(response.status_code)
-
-
-
-        response = client.get('/limitedtb', headers={'x-test-ip': ip})
-        res.append(response.status_code)
-
-        # Check that 'res' consists of 10 '200' codes and 10 '429' codes.
-        self.assertEqual(res.count(200), 10)
-        self.assertEqual(res.count(429), 2)
+		
+		for _ in range(8):
+			response = client.get('/limitedtb', headers={'x-test-ip': ip2})
+			res.append(response.status_code)
+		
+		self.assertEqual(res.count(200), 18)
+		self.assertEqual(res.count(429), 1)
